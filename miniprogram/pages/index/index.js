34d36ea4865b5011bb956a86205e0cd5ed294f85
yinhelaoxian <15963286721@163.com>
2025-08-29 16:11:08 +0800

    Update files:2025年 8月29日 星期五 16时11分08秒 CST

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -1,104 +1,41 @@
-import WeCropper from '../../components/we-cropper/we-cropper.js';
-
 Page({
-  data: {
-    cropperOpt: {
-      id: 'cropper', // canvas ID，必须与 wxml 中的 canvas-id 一致
-      width: 0, // 动态设置
-      height: 0, // 动态设置
-      src: '', // 图片路径
-      scale: 2.5, // 缩放比例
-      zoom: 8, // 缩放系数
-      cut: {
-        x: 0, // 裁剪框 x 坐标
-        y: 0, // 裁剪框 y 坐标
-        width: 200, // 裁剪框宽度
-        height: 200 // 裁剪框高度
-      }
-    }
-  },
+  data:{
+   imageSrc: '',
+   showCropper: false,
+   croppedImage: ''
+ },
 
-  onLoad() {
-    // 使用异步 API 获取设备信息
-    wx.getSystemSetting({
-      success: setting => {
-        wx.getWindowInfo({
-          success: window => {
-            this.setData({
-              'cropperOpt.width': window.windowWidth,
-              'cropperOpt.height': window.windowHeight * 0.6,
-              'cropperOpt.pixelRatio': window.pixelRatio || 1
-            });
-            console.log('cropperOpt:', this.data.cropperOpt);
-            const { cropperOpt } = this.data;
-            this.cropper = new WeCropper(cropperOpt)
-              .on('ready', () => {
-                console.log('裁剪框初始化完成');
-              })
-              .on('imageLoad', () => {
-                console.log('图片加载完成');
-              })
-              .on('error', err => {
-                console.error('裁剪组件错误:', err);
-              });
-          },
-          fail: err => {
-            console.error('获取窗口信息失败:', err);
-            // 回退到默认值
-            this.setData({
-              'cropperOpt.width': 300,
-              'cropperOpt.height': 300
-            });
-            const { cropperOpt } = this.data;
-            this.cropper = new WeCropper(cropperOpt);
-          }
-        });
-      },
-      fail: err => {
-        console.error('获取系统设置失败:', err);
-      }
-    });
-  },
+ chooseImage() {
+   wx.chooseImage({
+     count: 1,
+     success: (res) => {
+       this.setData({
+         imageSrc: res.tempFilePaths[0],
+         showCropper: true
+       });
+     }
+   });
+ },
 
-  chooseImage() {
-    wx.chooseImage({
-      count: 1,
-      sizeType: ['compressed'],
-      sourceType: ['album', 'camera'],
-      success: res => {
-        const src = res.tempFilePaths[0];
-        wx.getImageInfo({
-          src: src,
-          success: info => {
-            console.log('图片信息:', info);
-            this.setData({
-              'cropperOpt.src': src
-            });
-            this.cropper.updateCanvas();
-          },
-          fail: err => {
-            console.error('获取图片信息失败:', err);
-          }
-        });
-      },
-      fail: err => {
-        console.error('选择图片失败:', err);
-      }
-    });
-  },
+ onConfirmCrop() {
+   this.selectComponent('.we-cropper').getCropperImage();
+ },
 
-  getCropperImage() {
-    console.log('开始裁剪:', this.cropper);
-    this.cropper.getCropperImage(res => {
-      console.log('裁剪结果:', res);
-      if (res && res.tempFilePath) {
-        console.log('裁剪后的图片路径:', res.tempFilePath);
-        wx.previewImage({
-          urls: [res.tempFilePath]
-        });
-      } else {
-        console.error('裁剪失败:', res);
-      }
-    });
-  }
+ onCancelCrop() {
+   this.setData({ showCropper: false });
+ },
+
+ onCropConfirm(e) {
+   const tempFilePath = e.detail.tempFilePath;
+   this.setData({
+     croppedImage: tempFilePath,
+     showCropper: false
+   });
+   wx.showToast({ title: '裁剪成功' });
+ },
+
+ onCropCancel() {
+   this.setData({ showCropper: false });
+   wx.showToast({ title: '已取消', icon: 'none' });
+ }
 });
